69bcafd4be78e871f831b8ce0a4ca4ae81616cd4
Riles <railas.benoun92@gmail.com>
2021-12-12 07:26:14 +0100

    good

--- toDoListItem.js
+++ toDoListItem.js
@@ -1,26 +1,26 @@
-import React, { useCallback } from 'react';
+import React, {useCallback} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import {
-  StyleSheet,
-  View,
-  Text,
-  Button
-} from 'react-native';
-import { useEffect } from 'react/cjs/react.development';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 const ToDoListItem = ({tab, updateList, toDoList, index}) => {
-    
-    const deleteItem = useCallback((e)=>{ 
-      toDoList.splice(e,1)
-      updateList([...toDoList])
-    },[toDoList])
+  const setData = async () => {
+    const value = JSON.stringify(toDoList);
+    AsyncStorage.setItem('toDoList', value);
+  };
+  const deleteItem = useCallback(
+    e => {
+      toDoList.splice(e, 1);
+      updateList([...toDoList]);
+      setData();
+    },
+    [toDoList]
+  );
 
-    
   return (
     <View style={styles.div}>
-        <Text>{tab}</Text>
-        <Button title = 'delete' onPress={() =>deleteItem(index)}/>
+      <Text>{tab}</Text>
+      <Button title="delete" onPress={() => deleteItem(index)} />
     </View>
   );
 };
@@ -28,9 +28,9 @@ const ToDoListItem = ({tab, updateList, toDoList, index}) => {
 const styles = StyleSheet.create({
   div: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between"
-  }
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
 
 export default ToDoListItem;
--- toDoListScreen.js
+++ toDoListScreen.js
@@ -1,108 +1,89 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import ToDoListItem from './toDoListItem';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  Image,
-  useColorScheme,
   View,
   TextInput,
   Button,
-  Alert,
   FlatList,
-  checkForm,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const ToDoListScreen = props => {
+  const {navigation} = props;
+  const [it, itemChange] = useState('');
+  const [toDoList, updateList] = useState([]);
+  const [filtered, updateFiltered] = useState('');
 
-const ToDoListScreen = (props) => {
-  
-  const {navigation} = props
-  const [item, itemChange] = useState('');
-  const [toDoList, updateList] = useState([])
-  const [filtered, updateFiltered] = useState('')
-
-  
   const getData = async () => {
-   try{
-      const value = await AsyncStorage.getItem('toDoList')
-      console.log(JSON.parse(value))
-      return JSON.parse(value)
-   }
-    catch(e){
-      console.error(e)
+    try {
+      const value = await AsyncStorage.getItem('toDoList');
+      console.log(JSON.parse(value));
+      updateList(JSON.parse(value));
+    } catch (e) {
+      console.error(e);
     }
-  }
+  };
 
   const setData = async () => {
+    const value = JSON.stringify(toDoList);
+    AsyncStorage.setItem('toDoList', value);
+  };
 
-      const value = JSON.stringify(toDoList)
-       AsyncStorage.setItem('toDoList', value)
-   }
-  
-  
-  function checkForm (){
-      updateList([...toDoList, item])
+  function checkForm() {
+    updateList([...toDoList, it]);
+    setData();
   }
-  useEffect(()=>{
-    if (toDoList.length != 0){
-      setData();
-      getData();
-    }
+  useEffect(() => {
+    getData();
+  }, []);
 
-  },[toDoList])
-  
-  const goToItem = useCallback((i)=>{
-    navigation.navigate('item', {item : i})
-  },[navigation])
-
-
+  const goToItem = useCallback(
+    i => {
+      navigation.navigate('item', {item: i});
+    },
+    [navigation],
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-    <View style={styles.container}>
+      <View style={styles.container}>
+        <View>
+          <TextInput
+            value={filtered}
+            placeholder="recherche"
+            onChangeText={updateFiltered}
+          />
+        </View>
 
-      <View >
+        <View>
+          <FlatList
+            data={toDoList}
+            renderItem={({item, index}) => {
+              return item.includes(filtered) ? (
+                <TouchableOpacity onPress={() => goToItem(item)}>
+                  <ToDoListItem
+                    tab={item}
+                    index={index}
+                    updateList={updateList}
+                    toDoList={toDoList}
+                  />
+                </TouchableOpacity>
+              ) : null;
+            }}
+          />
+        </View>
 
-        <TextInput 
-        value={filtered}
-        placeholder="recherche"
-        onChangeText={updateFiltered}
+        <TextInput
+          value={it}
+          placeholder="enter something"
+          onChangeText={itemChange}
         />
-       
+
+        <Button title="Press me" onPress={() => checkForm()} />
       </View>
-
-      <View>
-
-      <FlatList data={toDoList} renderItem={({item, index})=>{
-        return item.includes(filtered) ? 
-        <TouchableOpacity onPress={() => goToItem(item)}>
-        <ToDoListItem tab={item} index={index} updateList={updateList} toDoList={toDoList}/> 
-        </TouchableOpacity> : null
-      }}
-       />
-      </View>
-
-      <TextInput 
-      value = {item}
-      placeholder = 'enter something'
-      onChangeText = {itemChange}
-      />
-
-      <Button title="Press me" onPress={() => checkForm()} />
-
-    </View>
     </SafeAreaView>
   );
 };
@@ -110,11 +91,11 @@ const ToDoListScreen = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
-  inputContainer:{
-    flex:1
-  }
+  inputContainer: {
+    flex: 1,
+  },
 });
 
 export default ToDoListScreen;
